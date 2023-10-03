966fae65e759f3d9f2d50db1b79baa89cf8ea82a
Valentin Kuznetsov <vkuznet@gmail.com>
2023-10-03 11:22:59 -0400

    Add HttpXXX functions

--- utils/http.go
+++ utils/http.go
@@ -1,12 +1,18 @@
 package utils
 
 import (
+	"bytes"
 	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 	"os"
 	"strings"
 	"time"
 )
+
+// Verbose controls verbosity level of function printouts
+var Verbose int
 
 // ReadToken function to either read file content or return given string
 func ReadToken(r string) string {
@@ -27,4 +33,70 @@ func HttpClient() *http.Client {
 		return &http.Client{Timeout: time.Duration(timeout)}
 	}
 	return &http.Client{}
+}
+
+// HttpGet performs HTTP GET request with bearer token
+func HttpGet(rurl string, headers map[string]string) (*http.Response, error) {
+	req, err := http.NewRequest("GET", rurl, nil)
+	if err != nil {
+		return nil, err
+	}
+	for k, v := range headers {
+		req.Header.Add(k, v)
+	}
+	client := &http.Client{}
+	if Verbose > 1 {
+		dump, err := httputil.DumpRequestOut(req, true)
+		log.Println("request", string(dump), err)
+	}
+	resp, err := client.Do(req)
+	if Verbose > 1 {
+		dump, err := httputil.DumpResponse(resp, true)
+		log.Println("response", string(dump), err)
+	}
+	return resp, err
+}
+
+// HttpPost performs HTTP POST request with bearer token
+func HttpPost(rurl string, headers map[string]string, buffer *bytes.Buffer) (*http.Response, error) {
+	req, err := http.NewRequest("POST", rurl, buffer)
+	if err != nil {
+		return nil, err
+	}
+	for k, v := range headers {
+		req.Header.Add(k, v)
+	}
+	client := &http.Client{}
+	if Verbose > 1 {
+		dump, err := httputil.DumpRequestOut(req, true)
+		log.Println("request", string(dump), err)
+	}
+	resp, err := client.Do(req)
+	if Verbose > 1 {
+		dump, err := httputil.DumpResponse(resp, true)
+		log.Println("response", string(dump), err)
+	}
+	return resp, err
+}
+
+// HttpPostForm performs HTTP POST form request with bearer token
+func HttpPostForm(rurl string, headers map[string]string, formData url.Values) (*http.Response, error) {
+	req, err := http.NewRequest("POST", rurl, strings.NewReader(formData.Encode()))
+	if err != nil {
+		return nil, err
+	}
+	for k, v := range headers {
+		req.Header.Add(k, v)
+	}
+	client := &http.Client{}
+	if Verbose > 1 {
+		dump, err := httputil.DumpRequestOut(req, true)
+		log.Println("request", string(dump), err)
+	}
+	resp, err := client.Do(req)
+	if Verbose > 1 {
+		dump, err := httputil.DumpResponse(resp, true)
+		log.Println("response", string(dump), err)
+	}
+	return resp, err
 }
